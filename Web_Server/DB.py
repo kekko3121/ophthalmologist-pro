0db855afc1a7ae7d6fa773fb28986fe0ee0c77aa
kekko3121 <francescopiogravino31@gmail.com>
2023-12-21 12:02:21 +0100

    Changes to be committed: 	modified:   Database/DBOPHTHALMOLOGIST.sql 	modified:   Web_Server/DB.py 	modified:   Web_Server/templates/homepage.html

--- Web_Server/DB.py
+++ Web_Server/DB.py
@@ -34,7 +34,11 @@ class DB():
     
     def set_prescription(self, data):
         try: 
-            self.cursor.execute("INSERT INTO PATIENT VALUES (:cf, :firstname, :surname)", cf = data.get("cf"), firstname = data.get("firstname"), surname = data.get("lastname"))
+            self.cursor.execute("""INSERT INTO PRESCRIPTION (PRESCRIPTION_DATE, LEFT_AXLE, RIGHT_AXLE, LEFT_SPHERE, 
+                                RIGHT_SPHERE, LEFT_CYLINDER, RIGHT_CYLINDER, PRESCRIPTION_DUR, INTERPUPILLARY_DIST, 
+                                SEMIDAV, CORNEAL_APEX_DIST, PANTHOSCOPIC_ANGLE, INSET, LENS_TYPE, TREATMENT, CF_PAZ) 
+                                VALUES (SYSDATE, :left_axle, :right_axle, :left_sphere, :right_sphere, :left_cylinder, :right_cylinder,
+                                ,)""", cf = data.get("cf"), firstname = data.get("firstname"), surname = data.get("lastname"))
             self.conn.commit()
         except Exception as e:
             self.conn.rollback()  # Roll back the transaction in case of an error.
