ff77eddde8c181ea18fe74edcaba0a0e0170d6ef
kekko3121 <francescopiogravino31@gmail.com>
2024-01-06 14:42:43 +0100

    Changes to be committed: 	new file:   Web_Server/static/js/service-worker.js 	new file:   Web_Server/static/manifest.json 	modified:   Web_Server/templates/index.html

---
+++ Web_Server/static/js/service-worker.js
@@ -0,0 +1,18 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('app-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/static/css/style.css',
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
